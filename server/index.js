f147958cce8c6ffa15285a3157a915dbe9c0dfe2
Adam Harpur <adam@harpur.io>
2019-02-24 10:40:06 -0600

    feat: added to oauth flow

--- server/index.js
+++ server/index.js
@@ -9,8 +9,10 @@ passport.use(
         clientID: keys.googleClientID,
         clientSecret: keys.googleClientSecret,
         callbackURL: '/auth/google/callback'
-    }, accessToken => {
-        console.log(accessToken);
+    }, (accessToken, refreshToken, profile, done) => {
+        console.log('accessToken', accessToken);
+        console.log('refreshToken', refreshToken);
+        console.log('profile', profile);
     })
 );
 
@@ -20,5 +22,6 @@ app.get('/auth/google',
     })
 );
 
+app.get('/auth/google/callback', passport.authenticate('google'))
 const PORT = process.env.PORT || 5000;
 app.listen(PORT);
