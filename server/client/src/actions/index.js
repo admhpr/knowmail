260db6885c8e399a33c6ed8063dfb71653f4fabd
Adam Harpur <adam@harpur.io>
2019-03-14 21:03:10 -0500

    refactor: only return res data

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -4,8 +4,11 @@ import {
 } from './types';
 
 
-export const fetchUser = () => async dispatch =>
-    dispatch({
-        type: FETCH_USER,
-        payload: await axios.get('/api/current_user')
-    })
+export const fetchUser = () =>
+    async dispatch => {
+        const res = await axios.get('/api/current_user');
+        dispatch({
+            type: FETCH_USER,
+            payload: res.data
+        })
+    }
