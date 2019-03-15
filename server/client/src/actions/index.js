9862b06b0bc3805fa9b07b7128945f7eaa9b0b7d
Adam Harpur <adam@harpur.io>
2019-03-14 20:59:13 -0500

    refactor: action creators

--- server/client/src/actions/index.js
+++ server/client/src/actions/index.js
@@ -4,14 +4,8 @@ import {
 } from './types';
 
 
-export const fetchUser = () => {
-    return function (dispatch) {
-        axios.get('/api/current_user')
-            .then(res => dispatch({
-                type: FETCH_USER,
-                payload: res
-            }))
-
-    }
-
-}
+export const fetchUser = () => async dispatch =>
+    dispatch({
+        type: FETCH_USER,
+        payload: await axios.get('/api/current_user')
+    })
