744f81b47da0ad42891e235ff4622cfeccc39aec
Adam Harpur <adam@harpur.io>
2019-03-14 20:26:38 -0500

    style: header

--- server/client/src/components/Header.jsx
+++ server/client/src/components/Header.jsx
@@ -2,6 +2,17 @@ import React, { Component } from "react";
 
 export default class Header extends Component {
   render() {
-    return <div>Header</div>;
+    return (
+      <nav>
+        <div className="nav-wrapper">
+          <a className="left brand-logo">knowmail</a>
+          <ul className="right">
+            <li>
+              <a href="">Login With Google</a>
+            </li>
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
