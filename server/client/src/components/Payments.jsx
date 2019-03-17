f384d71d6865329f9b5297657d1952900fa278a1
Adam Harpur <adam@harpur.io>
2019-03-17 09:01:53 -0500

    feat: payments name

--- server/client/src/components/Payments.jsx
+++ server/client/src/components/Payments.jsx
@@ -5,6 +5,8 @@ class Payments extends Component {
   render() {
     return (
       <StripeCheckout
+        name="KnowMail"
+        description="$5 for 5 credits"
         amount={500}
         token={token => console.log(token)}
         stripeKey={process.env.REACT_APP_STRIPE_PUB_KEY}
