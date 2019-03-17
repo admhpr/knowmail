6615ad80aae202de720f01e793c891619c9d20cd
Adam Harpur <adam@harpur.io>
2019-03-17 13:01:26 -0500

    feat: finished stripe hook up

--- server/routes/payments.js
+++ server/routes/payments.js
@@ -2,7 +2,17 @@ const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
 
 
 module.exports = app => {
-    app.post('/api/stripe', (req, res) => {
+    app.post('/api/stripe', async (req, res) => {
+        const {
+            body
+        } = req;
+        const charge = await stripe.charges.create({
+            amount: 500,
+            currency: 'usd',
+            description: 'credit purchase',
+            source: body.id,
+        })
 
+        console.log(charge)
     })
 }
