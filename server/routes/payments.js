c65c2dc1836d731a02ae57852bff8be3c6a8b13c
Adam Harpur <adam@harpur.io>
2019-03-17 13:19:18 -0500

    feat: authorization for endpoints

--- server/routes/payments.js
+++ server/routes/payments.js
@@ -1,18 +1,27 @@
 const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY)
-
+const requireLogin = ('../middlewares/requireLogin');
 
 module.exports = app => {
-    app.post('/api/stripe', async (req, res) => {
+    app.post('/api/stripe', requireLogin, async (req, res) => {
         const {
             body
         } = req;
-        const charge = await stripe.charges.create({
-            amount: 500,
-            currency: 'usd',
-            description: 'credit purchase',
-            source: body.id,
-        })
+        try {
+            const charge = await stripe.charges.create({
+                amount: 500,
+                currency: 'usd',
+                description: 'credit purchase',
+                source: body.id,
+            })
 
 
+            req.user.credits += 5;
+
+            const user = await req.user.save();
+            res.send(user)
+
+        } catch (e) {
+            console.log(e.message)
+        }
     })
 }
