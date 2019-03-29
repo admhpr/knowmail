50705931dad98bd356aa1778d08f0f95617bf480
Adam Harpur <adam@harpur.io>
2019-03-29 09:02:05 -0500

    doc: example of broken code

--- server/services/Mailer.js
+++ server/services/Mailer.js
@@ -1,28 +1,51 @@
-const sgMail = require('@sendgrid/mail');
+const sendgrid = require("sendgrid");
+const helper = sendgrid.mail;
 
-class Mailer {
-    constructor({
-        subject,
-        recipients
-    }, content) {
-        sgMail.setApiKey(process.env.SENDGRID_KEY);
-        this.to = recipients.map(({
-            email
-        }) => email);
-        this.from = 'no-reply@knowmail.com';
-        this.subject = subject;
-        this.html = content;
-    }
+class Mailer extends helper.Mail {
+  constructor({ subject, recipients }, content) {
+    super();
 
-    async send() {
-        let response;
-        try {
-            response = await sgMail.send(this, true);
-        } catch (error) {
-            console.error(error);
-        }
-        return response;
-    }
+    this.sendGridApi = sendgrid(process.env.SENDGRID_KEY);
+    this.from_email = new helper.Email("no-reply@knowmail.com");
+    this.subject = subject;
+    this.body = new helper.Content("text/html", content);
+    this.recipients = this.formatAddresses(recipients);
+
+    this.addContent(this.body);
+    this.addClickTracking();
+    this.addRecipients();
+  }
+
+  formatAddresses(recipients) {
+    return recipients.map(({ email }) => new helper.Email(email));
+  }
+
+  addClickTracking() {
+    const trackingSettings = new helper.TrackingSettings();
+    const clickTracking = new helper.ClickTracking(true, true);
+    trackingSettings.setClickTracking(clickTracking);
+    this.addTrackingSettings(trackingSettings);
+  }
+
+  addRecipients() {
+    const personalise = new helper.Personalization();
+    this.recipients.forEach(recipient => {
+      personalise.addTo(recipient);
+    });
+
+    this.addPersonalization(personalise);
+  }
+
+  async send() {
+    const request = this.sendGridApi.emptyRequest({
+      method: "POST",
+      path: "v3/mail/send",
+      body: this.toJSON()
+    });
+
+    const response = this.sendGridApi.API(request);
+    return response;
+  }
 }
 
 module.exports = Mailer;
