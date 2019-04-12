e7bdb9365ac893755f77c50b4ce402f85298b2e9
Adam Harpur <adam@harpur.io>
2019-04-12 13:34:11 -0500

    feat: added dashboard

--- server/client/src/components/App.jsx
+++ server/client/src/components/App.jsx
@@ -2,10 +2,9 @@ import React, { Component } from "react";
 import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./Header";
 import Landing from "./Landing";
+import Dashboard from "./Dashboard";
 import { connect } from "react-redux";
 import * as actions from "../actions";
-
-const Dashboard = () => <h2>Dashboard</h2>;
 const SurveryNew = () => <h2>SurveryNew</h2>;
 
 class App extends Component {
