3dbcca8da585c1a7ff32276f27759e7f7818d9dc
Adam Harpur <adam@harpur.io>
2019-04-14 16:15:53 -0500

    feat: new survey form hook up

--- server/client/src/components/App.jsx
+++ server/client/src/components/App.jsx
@@ -3,9 +3,9 @@ import { BrowserRouter, Route } from "react-router-dom";
 import Header from "./Header";
 import Landing from "./Landing";
 import Dashboard from "./Dashboard";
+import SurveyNew from "./surveys/SurveyNew";
 import { connect } from "react-redux";
 import * as actions from "../actions";
-const SurveryNew = () => <h2>Survery New</h2>;
 
 class App extends Component {
   componentDidMount() {
@@ -20,7 +20,7 @@ class App extends Component {
             <Header />
             <Route exact path="/" component={Landing} />
             <Route exact path="/surveys" component={Dashboard} />
-            <Route path="/surveys/new" component={SurveryNew} />
+            <Route path="/surveys/new" component={SurveyNew} />
           </div>
         </BrowserRouter>
       </div>
--- server/client/src/components/surveys/SurveyNew.jsx
+++ server/client/src/components/surveys/SurveyNew.jsx
@@ -2,6 +2,6 @@ import React, { Component } from "react";
 
 export default class SurveyNew extends Component {
   render() {
-    return <div />;
+    return <div>SurveyNew</div>;
   }
 }
