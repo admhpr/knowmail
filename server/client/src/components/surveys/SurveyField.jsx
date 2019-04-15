4cd2322c989486b5302cd49677eb6eb51b40551d
Adam Harpur <adam@harpur.io>
2019-04-15 17:36:06 -0500

    feat: more validation for form

--- server/client/src/components/surveys/SurveyField.jsx
+++ server/client/src/components/surveys/SurveyField.jsx
@@ -3,9 +3,11 @@ import React from "react";
 const SurveyField = ({ input, label, meta: { error, touched } }) => {
   return (
     <div>
-      <label htmlFor="input">{label}</label>
-      <input {...input} />
-      {touched && error}
+      <label htmlFor="input">{`${label}*`}</label>
+      <input {...input} style={{ marginBottom: "5px" }} />
+      <div className="red-text" style={{ marginBottom: "20px" }}>
+        {touched && error}
+      </div>
     </div>
   );
 };
