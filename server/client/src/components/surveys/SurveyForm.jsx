c4092d6b5b13dbec8869313c1adcf5576b760f0d
Adam Harpur <adam@harpur.io>
2019-04-15 17:18:33 -0500

    style: button

--- server/client/src/components/surveys/SurveyForm.jsx
+++ server/client/src/components/surveys/SurveyForm.jsx
@@ -29,7 +29,10 @@ class SurveyForm extends Component {
       <div>
         <form onSubmit={handleSubmit(values => console.log(values))}>
           {this.renderFields()}
-          <button type="submit">Submit</button>
+          <button className="teal btn-flat right white-text" type="submit">
+            Next <i className="material-icons right">done</i>
+          </button>
+          <button>Cancel</button>
         </form>
       </div>
     );
