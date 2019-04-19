503420780729df7549e6922d5b4a3aba82bf650c
Adam Harpur <adam@harpur.io>
2019-04-19 17:14:27 -0500

    feat: added path lib

--- server/routes/survey.js
+++ server/routes/survey.js
@@ -1,3 +1,8 @@
+const _ = require('lodash');
+const Path = require('path-parser');
+const {
+    URL
+} = require('url');
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
@@ -12,8 +17,11 @@ module.exports = app => {
     })
 
     app.post('/api/surveys/webhooks', (req, res) => {
-        console.log(req.body)
-        res.send({})
+        const events = _.map(req.body, event => {
+            const pathname = new URL(event.url).pathname;
+            const p = new Path('/api/surveys/:surveyId/:choice');
+
+        })
     })
     app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
         const {
