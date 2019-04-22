532d173bc3f709da5525d7db26c14e201804ad9c
Adam Harpur <adam@harpur.io>
2019-04-22 17:22:57 -0500

    feat: web hook logic for send grid

--- server/routes/survey.js
+++ server/routes/survey.js
@@ -1,76 +1,63 @@
-const _ = require('lodash');
-const {
-    Path
-} = require('path-parser');
-const {
-    URL
-} = require('url');
-const mongoose = require('mongoose');
-const requireLogin = require('../middlewares/requireLogin');
-const requireCredits = require('../middlewares/requireCredits');
-const Mailer = require('../services/Mailer')
-const surveyTemplate = require('../services/emailTemplates/survey')
+const _ = require("lodash");
+const { Path } = require("path-parser");
+const { URL } = require("url");
+const mongoose = require("mongoose");
+const requireLogin = require("../middlewares/requireLogin");
+const requireCredits = require("../middlewares/requireCredits");
+const Mailer = require("../services/Mailer");
+const surveyTemplate = require("../services/emailTemplates/survey");
 
-const Survey = mongoose.model('surveys');
+const Survey = mongoose.model("surveys");
 
 module.exports = app => {
-    app.get('/api/surveys/thanks', (req, res) => {
-        res.send('Thanks for voting!');
-    })
+  app.get("/api/surveys/thanks", (req, res) => {
+    res.send("Thanks for voting!");
+  });
 
-    app.post('/api/surveys/webhooks', (req, res) => {
-        const events = _.map(req.body, ({
-            email,
-            url
-        }) => {
-            const pathname = new URL(url).pathname;
-            const p = new Path('/api/surveys/:surveyId/:choice');
-            const match = p.test(pathname);
-            if (match) {
-                const {
-                    surveyId,
-                    choice
-                } = match;
-                return {
-                    email,
-                    surveyId,
-                    choice
-                };
-            }
-        })
+  app.post("/api/surveys/webhooks", (req, res) => {
+    const events = _.map(req.body, ({ email, url }) => {
+      const pathname = new URL(url).pathname;
+      const p = new Path("/api/surveys/:surveyId/:choice");
+      const match = p.test(pathname);
+      if (match) {
+        const { surveyId, choice } = match;
+        return {
+          email,
+          surveyId,
+          choice
+        };
+      }
+    });
+    const compactEvents = _.compact(events);
+    const uniqueEvents = _.uniqBy(compactEvents, "email", "id");
 
-        console.log(events);
-    })
-    app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
-        const {
-            title,
-            subject,
-            body,
-            recipients
-        } = req.body;
+    console.log(uniqueEvents);
+    res.send({});
+  });
+  app.post("/api/surveys", requireLogin, requireCredits, async (req, res) => {
+    const { title, subject, body, recipients } = req.body;
 
-        const survey = new Survey({
-            title,
-            subject,
-            body,
-            recipients: recipients.split(',').map(email => ({
-                email: email.trim()
-            })),
-            _user: req.user.id,
-            dateSent: Date.now(),
-        })
+    const survey = new Survey({
+      title,
+      subject,
+      body,
+      recipients: recipients.split(",").map(email => ({
+        email: email.trim()
+      })),
+      _user: req.user.id,
+      dateSent: Date.now()
+    });
 
-        const mailer = new Mailer(survey, surveyTemplate(survey));
+    const mailer = new Mailer(survey, surveyTemplate(survey));
 
-        try {
-            await mailer.send();
-            await survey.save();
-            req.user.credits -= 1;
-            await req.user.save();
-            res.send(req.user)
-
-        } catch (err) {
-            res.status(422).send(err.message)
-        }
-    })
-}
+    try {
+      await mailer.send();
+      await survey.save();
+      req.user.credits -= 1;
+      await req.user.save();
+      res.send(req.user);
+    } catch (err) {
+      res.status(422).send(err.message);
+    }
+  });
+};
