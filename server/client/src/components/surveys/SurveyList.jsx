8a3d9a5cc1dc5b9fb87d3daec853099325ca2df1
Adam Harpur <adam@harpur.io>
2019-04-25 13:56:08 -0500

    feat: survey card

---
+++ server/client/src/components/surveys/SurveyList.jsx
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { fetchSurveys } from "../../actions";
+
+class SurveyList extends Component {
+  componentDidMount() {
+    this.props.fetchSurveys();
+  }
+
+  renderSurveys(list) {
+    return list.map(survey => (
+      <div key={survey._id} className="card darken-1">
+        <div className="card-content">
+          <span className="card-title">{survey.title}</span>
+          <p>{survey.body}</p>
+          <div className="right">
+            Sent On: {new Date(survey.dateSent).toLocaleString()}
+          </div>
+        </div>
+        <div className="card-action">
+          <a>Yes: {survey.yes}</a>
+          <a>No: {survey.no}</a>
+        </div>
+      </div>
+    ));
+  }
+  render() {
+    const { surveys } = this.props;
+    return <div>{this.renderSurveys(surveys)}</div>;
+  }
+}
+
+// property from state that this component wants in props
+function mapStateToProps({ surveys }) {
+  return { surveys };
+}
+
+// mapper, action, component
+export default connect(
+  mapStateToProps,
+  { fetchSurveys }
+)(SurveyList);
